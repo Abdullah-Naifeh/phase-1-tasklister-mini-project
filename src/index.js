5ed09a4b0d7cb229e9549f7847d4692bf0a5800b
Abdullah <abdna59@gmail.com>
2022-12-04 06:57:01 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("create-task-form")
+  console.log(form)
+  form.addEventListener("submit" , (e) => {
+    e.preventDefault()
+    handleAddTask(e.target.new_task.value)
+    form.reset()
+
+  })
   // your code here
 });
+
+function handleAddTask(todo){
+  const li = document.createElement("li")
+  const btn = document.createElement("button")
+  const ul = document.getElementById("tasks")
+
+  li.textContent =todo;
+  btn.textContent = "✖";
+  ul.appendChild(li)
+  li.appendChild(btn)
+
+  btn.addEventListener("click", (e) => {e.target.parentNode.remove()})
+  
+  console.log(todo)
+}
